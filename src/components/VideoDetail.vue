e130859d2f96e71a70a574fe91a020461b9d9c53
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-11-19 16:11:31 +0200

    Add functionality to Video Details component

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-if="video">
-    {{ video.snippet.title }}
+  <div class="col-md-8" v-if="video">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe :src="videoUrl" class="embed-responsive-item"/>
+    </div>
+    <div class="details">
+      <h4>{{ video.snippet.title }}</h4>
+      <p>{{ video.snippet.description }}</p>
+    </div>
   </div>
 </template>
 
@@ -8,5 +14,20 @@
 export default {
   name: "VideoDetail",
   props: ["video"],
+  computed: {
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://www.youtube.com/embed/${videoId}`;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
