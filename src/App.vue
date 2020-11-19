a16f0d970dc7f7b2a79d62c56a5e0f95da9d0fe3
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-11-19 14:34:18 +0200

    Implement connection between parent and child components

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,43 @@
 <template>
   <div>
     <SearchBar @termChange="onTermChange"></SearchBar>
-    <VideoList></VideoList>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
 
-const API_KEY = 'AIzaSyCtgNtTWMSl2gpc6MwSsIzB3J5_b9UWaCQ';
+const API_KEY = "AIzaSyCtgNtTWMSl2gpc6MwSsIzB3J5_b9UWaCQ";
 
 export default {
   name: "App",
   components: {
     SearchBar,
-    VideoList
+    VideoList,
+  },
+  data() {
+    return {
+      videos: [],
+    };
   },
   methods: {
     onTermChange(searchTerm) {
-      axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          type: 'video',
-          part: 'snippet',
-          q: searchTerm
-        }
-      }).then(response => console.log(response))
-    }
-  }
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((response) => {
+          this.videos = response.data.items;
+        });
+    },
+  },
 };
 </script>
