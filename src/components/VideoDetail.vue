5530000ab2e23f5808dd750ce6b48358068c41d7
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-11-19 15:47:03 +0200

    Fix error

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="video">
     {{ video.snippet.title }}
   </div>
 </template>
@@ -7,6 +7,6 @@
 <script>
 export default {
   name: "VideoDetail",
-  props: ["video"]
+  props: ["video"],
 };
 </script>
