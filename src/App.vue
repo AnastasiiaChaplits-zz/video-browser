afa729e52446cb41455c96d26cc1823d30dd09f1
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2020-11-19 13:55:20 +0200

    Make request to youtube API

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    Hi there
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import SearchBar from "./components/SearchBar";
+const API_KEY = 'AIzaSyCtgNtTWMSl2gpc6MwSsIzB3J5_b9UWaCQ';
+
 export default {
   name: "App",
+  components: {
+    SearchBar,
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => console.log(response))
+    }
+  }
 };
 </script>
